a2d30081e791476c08f3015f3d6a92dc9313b39f
Antistas <xzeitz@yandex.ru>
2026-06-06 17:06:53 +0300

    checkstyle

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface ItemService {
     ItemDto create(Long userId, ItemDto itemDto);
+
     ItemDto update(Long userId, Long itemId, ItemDto itemDto);
+
     ItemDto getById(Long userId, Long itemId);
+
     Collection<ItemDto> getOwnerItems(Long userId);
+
     Collection<ItemDto> search(Long userId, String text);
 }
